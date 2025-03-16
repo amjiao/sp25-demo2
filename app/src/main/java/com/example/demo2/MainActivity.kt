2f20962432ada812ba2b5b73a11210ddd0e9b073
amjiao <amyjiaowang@gmail.com>
2025-03-16 15:49:04 -0400

    intial commit

--- app/src/main/java/com/example/demo2/MainActivity.kt
+++ app/src/main/java/com/example/demo2/MainActivity.kt
@@ -1,20 +1,17 @@
 package com.example.demo2
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.annotation.RequiresApi
+import com.example.demo2.demo2.TodoList
 import com.example.demo2.ui.theme.Demo2Theme
 
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/demo2/demo2/TodoList.kt
+++ app/src/main/java/com/example/demo2/demo2/TodoList.kt
@@ -1,4 +1,4 @@
-package com.example.demo2
+package com.example.demo2.demo2
 
 import android.os.Build
 import androidx.annotation.RequiresApi
